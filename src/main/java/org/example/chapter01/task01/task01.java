005af3a0a7f00e068ef51bb858bda3c34cc2f371
chesnokov-an <chesnokov-an-2017@mail.ru>
2026-02-18 20:41:59 +0300

    Fixed chapter01.task01

--- src/main/java/org/example/chapter01/task01/task01.java
+++ src/main/java/org/example/chapter01/task01/task01.java
@@ -1,33 +1,31 @@
 package org.example.chapter01.task01;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.lang.Exception;
 
 public class task01 {
     public static void main(String[] args){
         int num = 0;
         boolean flag = false;
+        Scanner in = new Scanner(System.in);
         while(!flag) {
-            Scanner in = new Scanner(System.in);
             System.out.print("Enter a number: ");
-            try {
+            if (!in.hasNext()) {
+                System.out.println("End of input");
+                break;
+            }
+            if (in.hasNextInt()) {
                 num = in.nextInt();
                 flag = true;
-            } catch (InputMismatchException e) {
+            } else {
                 System.out.println("Not a number entered");
-            } catch (NoSuchElementException e) {
-                break;
-            } catch (Exception e) {
-                System.out.println("Other problem");
-                break;
+                in.next();
             }
         }
         if(flag){
             System.out.println("bin: " + Integer.toString(num, 2));
             System.out.println("oct: " + Integer.toString(num, 8));
             System.out.println("hex: " + Integer.toString(num, 16));
+            System.out.printf("reciprocal hex: %a", 1/(double)num);
         }
     }
 }
